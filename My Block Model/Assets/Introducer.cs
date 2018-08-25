9e03b4c3324a106c6adf67f27efc0d075806b8de
darkhunterbg <kaloyan.r.nachev@gmail.com>
2018-08-26 01:17:37 +0300

    My Block Model

---
+++ My Block Model/Assets/Introducer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introducer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("START TEST!");
+
+        List<int> test = new List<int>();
+	}
+
+    void Test(ref int a)
+    {
+        a = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
