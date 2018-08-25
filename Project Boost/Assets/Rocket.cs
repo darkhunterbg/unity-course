cd510f4042c1b87d0dceb31f10c5cb4d6d096777
darkhunterbg <kaloyan.r.nachev@gmail.com>
2018-08-26 02:52:29 +0300

    Ship controlling

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcesInput();
+    }
+
+    private void ProcesInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward, 10 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward, -10 * Time.deltaTime);
+        }
+    }
+}
