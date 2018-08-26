7aca11f4ca1086289ec14a82c4aeb2f09d27d56d
darkhunterbg <kaloyan.r.nachev@gmail.com>
2018-08-26 22:55:25 +0300

    splash screen

---
+++ Argon Assault/Assets/SplashScreenChanger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DisallowMultipleComponent]
+public class SplashScreenChanger : MonoBehaviour {
+
+    [SerializeField] float delay;
+    [SerializeField] string sceneName;
+
+	// Use this for initialization
+	void Start () {
+        Invoke(nameof(LoadLevel), delay);	
+	}
+
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
