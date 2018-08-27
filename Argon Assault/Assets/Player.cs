939b48a2b735bb0b2493492c7af34bc0cfc236d7
Kaloyan Nachev <kaloyan.nachev@icb.bg>
2018-08-27 09:25:09 +0300

    clamp movement

--- Argon Assault/Assets/Player.cs
+++ Argon Assault/Assets/Player.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-   [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10.0f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10.0f;
     [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4.0f;
 
+    [SerializeField] Vector2 clamp = new Vector2(5, 2);
+
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float yOffset = yThrow * ySpeed * Time.deltaTime;
 
-        transform.localPosition += new Vector3(xOffset, yOffset, 0);
-	}
+        float rawNewXPos = Mathf.Clamp(transform.localPosition.x + xOffset, -clamp.x, clamp.x);
+        float rawNewYPos = Mathf.Clamp(transform.localPosition.y + yOffset, -clamp.y, clamp.y);
+
+        transform.localPosition = new Vector3(rawNewXPos, rawNewYPos, transform.localPosition.z);
+    }
 }
