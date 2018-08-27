ca4b3201b500d160d6f182a2f0ebc6de3ae3351f
Kaloyan Nachev <kaloyan.nachev@icb.bg>
2018-08-27 09:16:25 +0300

    ship control

---
+++ Argon Assault/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+   [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10.0f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        transform.localPosition += new Vector3(xOffset, yOffset, 0);
+	}
+}
