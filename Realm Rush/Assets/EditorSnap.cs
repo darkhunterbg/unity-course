33a68663023b3256fb36934bada6b26caa515cc5
Kaloyan Nachev <kaloyan.nachev@icb.bg>
2018-09-03 10:12:58 +0300

    Adde project Realm Rush with cube and grind snapping

---
+++ Realm Rush/Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+    [SerializeField, Range(1.0f, 20.0f)]
+    float gridSize = 10.0f;
+
+    void Update () {
+        Vector3 snapPos = (transform.position / gridSize) ;
+        snapPos.x = Mathf.RoundToInt(snapPos.x);
+        snapPos.y = Mathf.RoundToInt(snapPos.y);
+        snapPos.z = Mathf.RoundToInt(snapPos.z);
+        snapPos *= gridSize;
+        transform.position = snapPos;
+    }
+}
