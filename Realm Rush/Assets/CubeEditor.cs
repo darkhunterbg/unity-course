dc7c82c632b15af58417dec564edaf6bdd0507d9
Kaloyan Nachev <kaloyan.nachev@icb.bg>
2018-09-03 10:32:55 +0300

    grid text

--- Realm Rush/Assets/CubeEditor.cs
+++ Realm Rush/Assets/CubeEditor.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+public class CubeEditor : MonoBehaviour {
     [SerializeField, Range(1.0f, 20.0f)]
     float gridSize = 10.0f;
 
+    TextMesh textMesh;
+
+    private void Start()
+    {
+
+        
+    }
+
     void Update () {
+   
         Vector3 snapPos = (transform.position / gridSize) ;
         snapPos.x = Mathf.RoundToInt(snapPos.x);
-        snapPos.y = Mathf.RoundToInt(snapPos.y);
+        snapPos.y = 0;
         snapPos.z = Mathf.RoundToInt(snapPos.z);
-        snapPos *= gridSize;
-        transform.position = snapPos;
+        transform.position = snapPos * gridSize;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = $"{snapPos.x },{snapPos.z}";
+
     }
 }
